a06d8a0629cc18047f607e316a8768eba48fd708
Rerenah <60529747+Rerenah@users.noreply.github.com>
2020-02-02 11:16:18 -0500

    animation at the beginning

---
+++ Submarine_New/Assets/bobbingScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bobbingScript : MonoBehaviour
+{
+    public Transform sub;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sub = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
