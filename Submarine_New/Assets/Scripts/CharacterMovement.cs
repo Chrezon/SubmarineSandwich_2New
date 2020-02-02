d3de861def1afd667a64882ee38bec45c467fac0
wendidy <helianyeshui@gmail.com>
2020-02-02 10:55:56 -0500

    character movement???

--- Submarine_New/Assets/Scripts/CharacterMovement.cs
+++ Submarine_New/Assets/Scripts/CharacterMovement.cs
@@ -4,35 +4,27 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    //CharacterController chara;
     public float speed = 4f;
-    public Animator animator; 
+    public Animator animator;
     float xInput = 0f;
     float yInput = 0f;
 
+    //private Vector3 moveDirection = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
 
-        // Code to take care of animations
-        /*
-       if (xInput != 0f)
-        {
-            animator.SetFloat("SPEED", 0f);
-            animator.SetFloat("SPEED", xInput);
-        } else
-        {
-            animator.SetFloat("SPEED", 0f);
-            animator.SetFloat("SPEED", yInput);
-        }
-       */
+
         if (xInput != 0 || yInput != 0)
         {
             animator.SetFloat("SPEED", 10f);
@@ -41,77 +33,14 @@ public class CharacterMovement : MonoBehaviour
         {
             animator.SetFloat("SPEED", 0f);
         }
-
-        bool check = false;
-        if (xInput == -1 && !check)
-        {
-            if (yInput == -1)
-            {
-                transform.Rotate(Vector3.up * -135);
-                check = true;
-            }
-            else if (yInput == 0)
-            {
-                transform.Rotate(Vector3.up * -90);
-                check = true;
-            }
-            else
-            {
-                transform.Rotate(Vector3.up * -45);
-                check = true;
-            }
-        }
-        else if (xInput == 1 && !check)
-        {
-            if (yInput == -1)
-            {
-                transform.Rotate(Vector3.up * 135);
-                check = true;
-            }
-            else if (yInput == 0)
-            {
-                transform.Rotate(Vector3.up * 90);
-                check = true;
-            }
-            else
-            {
-                transform.Rotate(Vector3.up * 45);
-                check = true;
-            }
-        }
-        else if (xInput == 0 && !check)
-        {
-            if (yInput == -1)
-            {
-                transform.Rotate(Vector3.up * 180);
-                check = true;
-            }
-            else if (yInput == 1)
-            {
-                transform.Rotate(Vector3.up * 0);
-                check = true;
-            }
-        }
-        if (yInput == 0 && xInput == 0)
-        {
-            check = false;
-        }
-    }
-            
-        /*if (Input.GetButtonDown("left") && Input.GetButtonDown("up"))
-            RotateLeft();
     }
 
-        void RotateLeft()
-        {
-            Quaternion theRotation = transform.localRotation;
-            theRotation.z = 315;
-            transform.localRotation = theRotation;
-        }*/
-
-        private void FixedUpdate()
+    private void FixedUpdate()
     {
-			transform.Translate((new Vector3(xInput, yInput, 0f)).normalized * speed * Time.deltaTime);
+        float angle = Vector2.SignedAngle(Vector2.down, new Vector2(xInput, yInput));
+        Quaternion q = Quaternion.Euler(0f, 0f, angle);
+        this.transform.rotation = q;
+        transform.Translate((new Vector3(xInput, yInput, 0f)).normalized * speed * Time.deltaTime, Space.World);
     }
 
 }
