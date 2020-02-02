fe993858784dec151cfb6828a7e1a2660ab871d2
tylerstarr01 <tylerstarr@live.com>
2020-02-02 11:19:28 -0700

    Keyboard and Health Bar

---
+++ Submarine_New/Assets/LifeBarScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBarScript : MonoBehaviour
+{
+
+    public static int totalHealth = 3;
+    public const int threshold = 0;
+
+    public static int getHealth()
+    {
+        return totalHealth;
+    }
+
+    public static void decrementHealth()
+    {
+        totalHealth--;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void checkTaskFailed()
+    {
+
+        if (totalHealth <= threshold)
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+            gameOver();
+        }
+    }
+
+
+
+
+    void gameOver()
+    {
+
+
+        // Do Game Over Stuff
+
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        checkTaskFailed();
+
+
+    }
+}
--- Submarine_New/Assets/Scripts/consoleScript.cs
+++ Submarine_New/Assets/Scripts/consoleScript.cs
@@ -4,24 +4,57 @@ using UnityEngine;
 
 public class consoleScript : MonoBehaviour
 {
-
+    public static string consoleString = "";
+    public static string lastKey = "";
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnTriggerEnter2D();
     }
 
-    void OnTriggerEnter2D (Collider2D col)
+
+    void resetInput()
     {
-        if (col.CompareTag("Player"))
+        consoleString = "";
+    }
+
+
+    void OnTriggerEnter2D() //Collider2D col
+    {
+        bool antiSpam = false;
+
+
+        for (int i = 'a'; i < 'z'; i++)
         {
-            Debug.Log("player entered this area");
+            string casted = (char)i + "";
+            if (Input.GetKeyDown(casted))
+            {
+                consoleString += casted;
+                lastKey = casted;
+                antiSpam = true;
+            }
+
         }
+    }
+
+
+
+
+
+
+
+
+    Debug.Log(consoleString);
+        //if (col.CompareTag("Player"))
+       // {
+       //
+       //     Debug.Log("player entered this area");
+       // }
     }
 }
